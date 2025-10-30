49219715abf4dcdecdd55b9b238f733d91916efa
J <j.williams20@imperial.ac.uk>
2025-10-30 17:44:18 +0000

    Update 01b_MakeRegionalMap.r
    adjust to show south american outline and tropical forest ecoregions

--- 01b_MakeRegionalMap.r
+++ 01b_MakeRegionalMap.r
@@ -42,26 +42,37 @@ summary_df<-rbind(summary_df, c("Jurua", 30, -67.71, -5.56, 1)) %>%
          # Check
 print(summary_df)
 
-# --- base map: Brazil polygon as SpatVector ---
-brazil_sf  <- rnaturalearth::ne_countries(scale = "medium", country = "Brazil", returnclass = "sf")
-brazil_spv <- terra::vect(brazil_sf)  # SpatVector in EPSG:4326
+# --- new: get all South American countries instead of Brazil ---
+south_america_sf <- rnaturalearth::ne_countries(
+  continent = "South America", 
+  returnclass = "sf"
+)
+south_america_spv <- terra::vect(south_america_sf)
 
 
+
+
+# Load shapefile
+eco <- terra::vect("Data/Ecoregions2017/Ecoregions2017.shp")
+
+rainforest<-eco[eco$BIOME_NAME=="Tropical & Subtropical Moist Broadleaf Forests" & eco$REALM=="Neotropic"] %>% crop(south_america_spv)
 
 pts_spv <- vect(summary_df, geom = c("lon", "lat"), crs = "EPSG:4326")
+
+
 # --- Plot with tidyterra + ggplot2 ---
 p <- ggplot() +
-  # base: Brazil
-  geom_spatvector(data = brazil_spv, fill = "#757675ff", color = "#999999", size = 0.2) +
+  geom_spatvector(data = rainforest, fill = "grey", color = "grey", size = 0.2) +
+  geom_spatvector(data = south_america_spv, fill = NA , color = "black", linewidth = 0.4) +
   # underlay polygons (drawn first, semi-transparent)
   geom_spatvector(data = pts_spv,
                   aes(alpha = fc2000_mean, size=n) )+
   geom_spatvector(data = pts_spv,
-                  aes(size=n/2, color=Region)) +  
-    scale_size(range = c(30, 50))+
+                  aes(size=n/3, color=Region)) +  
+    scale_size(range = c(15, 35))+
   # numeric labels (n) on top
   geom_text(data = summary_df, aes(x = lon, y = lat, label = n),
-            color = "black", fontface = "bold", size=17) +
+            color = "black", fontface = "bold", size=8) +
   ggsci::scale_color_npg()+
   # coordinate limits focused on Brazil; tweak to adjust framing
   #coord_sf(xlim = c(-80, -40), ylim = c(-12, 2), expand = FALSE) +
@@ -77,4 +88,4 @@ p <- ggplot() +
 guides(size="none", alpha="none")
 
 p
-  ggsave("Figures/regions_map.png", p, width = 10, height = 10, dpi = 300)
+  ggsave("Figures/regions_map.png", p, width = 8, height = 12, dpi = 300, bg="white")
